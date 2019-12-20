b19c28366779781f8ea6b26e937c000f267eeef3
James Pace <jpace121@gmail.com>
2019-12-20 04:05:49 +0000

    Don't actually need all of that.

--- src/dev-contain-init.cpp
+++ src/dev-contain-init.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
-#include <csignal>
-
 #include <sys/wait.h>
-#include <signal.h>
-
-static std::sig_atomic_t m_wait = 0;
-
-//SIGCHILD 17
-void wait_child(int signal)
-{
-    m_wait++;
-}
 
 int main(int argc, char *argv[])
 {
-    std::signal(SIGCHLD, wait_child);
 
     while(true)
     {
-        if(m_wait > 0)
-        {
-            std::cout << "Reaping a process." << std::endl;
-            
-            int status;
-            waitpid(-1, &status, WNOHANG);
-            
-            m_wait--;
-        }
+        int status;
+        waitpid(-1, &status, WNOHANG);
     }
     
     return 0;
