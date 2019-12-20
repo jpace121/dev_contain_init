3f81bee200911413653bc21df0a837ad34a46011
James Pace <jpace121@gmail.com>
2019-12-20 04:56:13 +0000

    Add get boost process included. Not sure exactly where this is going...

--- src/dev-contain-init.cpp
+++ src/dev-contain-init.cpp
@@ -1,8 +1,11 @@
+#include <boost/process.hpp>
 #include <iostream>
 #include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
+
+    auto result = boost::process::system("echo 'Hi'");
 
     while(true)
     {
