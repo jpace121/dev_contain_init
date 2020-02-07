78480e3ad0eeb5f556eed0a7a7026d69c0b6a176
James Pace <jpace121@gmail.com>
2020-02-06 22:22:25 -0500

    With signals.

--- src/dev-contain-init.cpp
+++ src/dev-contain-init.cpp
@@ -1,17 +1,44 @@
-#include <boost/process.hpp>
+// Copyright 2019-2020 James Pace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #include <iostream>
 #include <sys/wait.h>
+#include <csignal>
+#include <unistd.h>
+
+void sigint_handler(int signal)
+{
+    kill(signal, -1);
+    std::quick_exit(1);
+}
+
+void sigchild_handler(int signal)
+{
+    int status;
+    waitpid(-1, &status, WNOHANG);
+}
 
 int main(int argc, char *argv[])
 {
-
-    auto result = boost::process::system("echo 'Hi'");
-
-    while(true)
+    if(not (getpid() == 1))
     {
-        int status;
-        waitpid(-1, &status, WNOHANG);
+        std::cerr << "dev_contain_init must be ran as pid 1. Exiting." << std::endl;
+        std::exit(-1);
     }
-    
+    std::signal(SIGINT, sigint_handler);
+    std::signal(SIGCHLD, sigchild_handler);
+
+    std::cout << "dev_contain_init running." << std::endl;
+    pause();
     return 0;
 }
